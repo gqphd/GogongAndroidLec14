f11f304645d776d5d5a4827dea9923d0e998b014
GQ <bispro89@gmail.com>
2014-08-13 01:23:50 +0900

    layout, sensor callback

--- (Sample)SensorInstrument/src/com/gqphd/samplesensorinstrument/MainActivity.java
+++ (Sample)SensorInstrument/src/com/gqphd/samplesensorinstrument/MainActivity.java
@@ -1,15 +1,90 @@
 package com.gqphd.samplesensorinstrument;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	LinearLayout m_linDbgValues;
+	TextView[] m_arrTvDbgVals;
+	
+	private SensorManager m_mgrSensor;
+	private Sensor m_sensorLinAcc;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		//init dbg ui
+		m_linDbgValues = (LinearLayout)findViewById(R.id.laylinAccval);
+		m_arrTvDbgVals = new TextView[4];
+		for(int i = 0 ; i <4; i++){
+			m_arrTvDbgVals[i] = new TextView(getApplicationContext());
+			m_arrTvDbgVals[i].setText(" VAL"+(i+1) + " ");
+			m_arrTvDbgVals[i].setTextColor(Color.BLACK);
+			m_linDbgValues.addView(m_arrTvDbgVals[i]);
+		}
+		
+		//init sensor
+		m_mgrSensor = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		m_sensorLinAcc = m_mgrSensor.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+	}
+	
+	SensorEventListener m_cbSensorListener = new SensorEventListener() {
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			if(Sensor.TYPE_LINEAR_ACCELERATION == event.sensor.getType()){
+				
+				//display acceleration values
+				for(int i = 0 ; i <3; i++){
+					String str = String.format(" %.02f ", event.values[i]);
+					m_arrTvDbgVals[i].setText(str);
+				}			
+				
+				//check if shaking
+				double pow = Math.sqrt(
+						event.values[0]*event.values[0] +
+						event.values[1]*event.values[1] +
+						event.values[2]*event.values[2] 
+						);
+				
+				double PowThreshold = 4.5;//heuristic
+				m_arrTvDbgVals[3].setText("POW " + String.format("%.2f", pow));
+				
+				if(pow > PowThreshold){
+					m_arrTvDbgVals[3].setTextColor(Color.RED);
+				}else{
+					m_arrTvDbgVals[3].setTextColor(Color.BLACK);
+				}
+				
+			}
+		}
+		
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			// TODO Auto-generated method stub
+			
+		}
+	};
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		m_mgrSensor.registerListener(m_cbSensorListener, m_sensorLinAcc, SensorManager.SENSOR_DELAY_FASTEST);
+	}
+	
+	@Override
+	protected void onPause() {
+		m_mgrSensor.unregisterListener(m_cbSensorListener);
+		super.onPause();
 	}
 }
